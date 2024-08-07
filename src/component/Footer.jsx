ac5621020ef3a216b1be9adfaa70cca4ff5f7b11
Jarvis9960 <ankitfukte11@gmail.com>
2024-08-08 02:31:47 +0530

    making changes to footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,46 +2,91 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-black text-white p-8">
-      <div className="flex flex-col md:flex-row justify-between items-center">
-        {/* 1st Section */}
-        <div className="text-center md:text-left ml-20 mb-4 md:mb-0 flex-1">
-          <div>HOME</div>
-          <div className=" mt-2 pt-2">ABOUT</div>
-          <div className=" mt-2 pt-2">PORTFOLIO</div>
-          <div className=" mt-2 pt-2 flex">PARTNERS</div>
-        </div>
-        <div className="text-center md:text-left mb-4 mr-60 md:mb-0 flex-1">
-          <div>HOME</div>
-          <div className=" mt-2 pt-2">ABOUT</div>
-        </div>{" "}
-        <div className="text-center md:text-left  mb-4 md:mb-0 flex-1">
-          <div className=" mr-60 ">HOME</div>
-        </div>
-        {/* 2nd Section */}
-        <div className="flex-1 flex flex-col justify-between">
-          <div className="text-center md:text-center">
-            <h1 className="font-semibold">Contact</h1>
-            <div className="border-t border-gray-600 mt-2 pt-2">
-              Email: example@example.com
+    <footer className="bg-black text-white py-10 px-6">
+      <div className=" flex justify-between">
+        <div className="flex-1 space-y-6 p-4">
+          <div className="text-3xl font-bold">LXRY</div>
+          <nav className="flex gap-40">
+            <div className="space-y-5">
+              <a href="/" className="block">
+                HOME
+              </a>
+              <a href="/about" className="block">
+                ABOUT
+              </a>
+              <a href="/process" className="block">
+                PROCESS
+              </a>
             </div>
-            <div className=" mt-2 pt-2">Address: 430 Wisconsin Ave</div>
-          </div>
-          <div className="text-center md:text-center mt-4 md:mt-8">
-            <h1 className="font-semibold ">Hours</h1>
-            <div className="border-t border-gray-600 mt-2 pt-2">
-              Monday-Friday: 9am - 5pm
+            <div className="space-y-5">
+              <a href="/contact" className="block">
+                CONTACT
+              </a>
+              <a href="/partners" className="block">
+                PARTNERS
+              </a>
             </div>
-            <div className="mt-2 pt-2">Saturday: 10am - 4pm</div>
-            <div className=" mt-2 pt-2">Sunday: Closed</div>
+            <div className="space-y-5">
+              <a href="/portfolio" className="block">
+                PORTFOLIO
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="flex-1 flex flex-col space-y-10 p-4">
+          <div className="space-y-6">
+            <div className="text-lg font-bold">CONTACT</div>
+            <div className="flex gap-10">
+              <div className="space-y-2">
+                <div>INFO@LXRYSTUDIO.COM</div>
+                <div>+202 796 1717</div>
+              </div>
+              <div>
+                2430 WISCONSIN AVE NW
+                <br />
+                WASHINGTON, D.C. 20007
+              </div>
+              <div className="flex space-x-4">
+                <a href="#">
+                  <img src="/path/to/instagram-icon.png" alt="Instagram" />
+                </a>
+                <a href="#">
+                  <img src="/path/to/linkedin-icon.png" alt="LinkedIn" />
+                </a>
+                <a href="#">
+                  <img src="/path/to/pinterest-icon.png" alt="Pinterest" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="space-y-6">
+            <div className="text-lg font-bold">HOURS</div>
+            <div className="flex gap-10">
+              <div className="space-y-2">
+                <div>
+                  MONDAY - FRIDAY
+                  <br />
+                  10:00 AM - 6:00 PM
+                </div>
+                <div>
+                  SUNDAY
+                  <br />
+                  *CLOSED*
+                </div>
+              </div>
+              <div>
+                SATURDAY
+                <br />
+                APPOINTMENT ONLY
+              </div>
+            </div>
           </div>
         </div>
-        {/* 3rd Section */}
       </div>
-      <div className="text-center md:text-left mt-4 md:mt-0 flex-1">
-        <div className="border-t border-gray-600 pt-2">© 2023 LXRY</div>
+      <div className="pt-5">
+        @2024 LXRY
       </div>
-    </div>
+    </footer>
   );
 }
 
